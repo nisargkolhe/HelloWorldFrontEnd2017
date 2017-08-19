d7e7197f309bc3a316a6b312d197dfa3acf502e6
avincent6 <vincent6@purdue.edu>
2017-08-18 23:02:38 -0500

    add login and logout to navbar and hide-show mobile nav onclick

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { UserService } from './services/index';
 
 @Component({
   moduleId: module.id,
@@ -7,6 +8,24 @@ import { Component, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./app.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  show: boolean;
+  public isLoggedIn: boolean;
+
+
+  constructor(
+    private userService: UserService
+  ) {
+    this.show = true;
+  }
   title = 'Hello World';
+  ngOnInit() {
+    if(this.userService.loadFromLocalStorage()){
+      //User is logged in
+      this.isLoggedIn = true;
+    } else {
+      //User is not logged in
+      this.isLoggedIn = false;
+    }
+  }
 }
